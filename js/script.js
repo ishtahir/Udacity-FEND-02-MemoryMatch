4914a0cf4fb2e8f1663f3e9a2846953d981c4492
Ish Tahir <ishtahir@icloud.com>
2018-06-02 19:48:19 -0500

    Linked the js file and declared my const variables

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// declare all const variables
+const emojis = ['😍', '😡', '😱', '🤢', '🤡', '💩', '💙', '😂'];
+const cards = document.querySelectorAll('.card');
+const board = document.querySelector('.board');
+const restartBtn = document.querySelector('.restart');
+const modal = document.querySelector('.modal');
+const playAgainBtn = document.querySelector('button');
+const starsPane = document.querySelectorAll('.fa-star');
+const timePane = document.querySelector('.time');
+const movesPane = document.querySelector('.moves');
+const message = document.querySelector('.message');
+const congrats = document.querySelector('.congrats');
