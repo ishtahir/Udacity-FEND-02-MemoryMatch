28d2eccc4ed8a70a5c22c8c406be77fcc112a18f
Ish Tahir <ishtahir@icloud.com>
2018-06-02 19:49:01 -0500

    Declared the let variables

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ const timePane = document.querySelector('.time');
 const movesPane = document.querySelector('.moves');
 const message = document.querySelector('.message');
 const congrats = document.querySelector('.congrats');
+
+// declare all let variables
+let stars = 3;
+let moves = 0;
+let matches = 0;
+let time = 0;
+let openCards = [];
+let startTime;
